88b7b9730cdc72a2318488297b2f04806f816e4a
Erosinho13 <56429945+Erosinho13@users.noreply.github.com>
2020-05-14 16:29:14 +0200

    Add files via upload

--- DANN.py
+++ DANN.py
@@ -6,15 +6,8 @@ try:
     from torch.hub import load_state_dict_from_url
 except ImportError:
     from torch.utils.model_zoo import load_url as load_state_dict_from_url
-
-
-__all__ = ['dann', 'DANN']
-
-
-model_urls = {
-    'alexnet': 'https://download.pytorch.org/models/alexnet-owt-4df8aa71.pth',
-}
-
+    
+from torchvision.models import alexnet
 
 class ReverseLayer(Function):
     
@@ -33,66 +26,19 @@ class ReverseLayer(Function):
 class DANN(nn.Module):
     
     
-    def __init__(self, num_classes = 1000):
+    def __init__(self, num_classes = 1000, pretrained = False):
         
         super(DANN, self).__init__()
+        alexnet_model = alexnet(pretrained = pretrained)
         
-        self.gf = nn.Sequential(
-            
-            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            
-            nn.Conv2d(64, 192, kernel_size=5, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            
-            nn.Conv2d(192, 384, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            
-            nn.Conv2d(384, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2)
-            
-        )
+        self.gf = alexnet_model.features
+        self.avgpool = alexnet_model.avgpool
+        self.gy = alexnet_model.classifier
+        self.gd = alexnet_model.classifier
         
-        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        self.gy[6] = nn.Linear(4096, num_classes)
+        self.gd[6] = nn.Linear(4096, 2)
         
-        self.gy = nn.Sequential(
-            
-            nn.Dropout(),
-            
-            nn.Linear(256 * 6 * 6, 4096),
-            nn.ReLU(inplace=True),
-            
-            nn.Dropout(),
-            
-            nn.Linear(4096, 4096),
-            nn.ReLU(inplace=True),
-            
-            nn.Linear(4096, num_classes)
-            
-        ) 
-        
-        self.gd = nn.Sequential(
-            
-            nn.Dropout(),
-            
-            nn.Linear(256 * 6 * 6, 4096),
-            nn.ReLU(inplace=True),
-            
-            nn.Dropout(),
-            
-            nn.Linear(4096, 4096),
-            nn.ReLU(inplace=True),
-            
-            nn.Linear(4096, 2)
-            
-        )
-
         
     def forward(self, x, alpha = None):
         
@@ -110,12 +56,8 @@ class DANN(nn.Module):
 
 
     
-def dann(pretrained = False, progress = True, **kwargs):
+def dann(pretrained = False, **kwargs):
     
-    model = DANN(**kwargs)
-    
-    if pretrained:
-        state_dict = load_state_dict_from_url(model_urls['alexnet'], progress = progress)
-        model.load_state_dict(state_dict, strict = False)
+    model = DANN(pretrained = pretrained, **kwargs)
         
     return model
